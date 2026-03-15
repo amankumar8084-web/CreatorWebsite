17f5562c264950c5b292cc78ec55bfad9900cb64
amankumar8084-web <amankumar8084227421@gmail.com>
2026-03-15 23:46:57 +0530

    vercel added

--- creator-website/backend/server.js
+++ creator-website/backend/server.js
@@ -6,7 +6,13 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://creator-website-five.vercel.app"
+  ]
+}));
+
 app.use(express.json());
 
 
